9bfe3c01ae2b616b7593d17313a01071b57f8ea1
BeJot94 <bartosz.jasinski.94@gmail.com>
2021-07-17 08:37:12 +0200

    Introducing CrudService into existing services

--- pet-clinic-data/src/main/java/pl/bejot/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/pl/bejot/sfgpetclinic/services/VetService.java
@@ -2,12 +2,5 @@ package pl.bejot.sfgpetclinic.services;
 
 import pl.bejot.sfgpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
