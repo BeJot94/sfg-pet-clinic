1ab3827c9890601a4b6f0dc8ca02ea5a8988b650
BeJot94 <bartosz.jasinski.94@gmail.com>
2021-07-17 16:06:40 +0200

    Adding Owners list display

--- pet-clinic-web/src/main/java/pl/bejot/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/pl/bejot/sfgpetclinic/controllers/OwnerController.java
@@ -1,14 +1,28 @@
 package pl.bejot.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.bejot.sfgpetclinic.model.Owner;
+import pl.bejot.sfgpetclinic.services.OwnerService;
+
+import java.util.Set;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/index", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+        Set<Owner> owners = ownerService.findAll();
+        model.addAttribute("owners", owners);
+
         return "/owners/index";
     }
 }
