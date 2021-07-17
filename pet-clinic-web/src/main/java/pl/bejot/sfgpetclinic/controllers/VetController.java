ba040fde6aee7de2fe366fa085b9f98455395b9f
BeJot94 <bartosz.jasinski.94@gmail.com>
2021-07-17 16:12:52 +0200

    Adding Vets list display

--- pet-clinic-web/src/main/java/pl/bejot/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/pl/bejot/sfgpetclinic/controllers/VetController.java
@@ -1,14 +1,28 @@
 package pl.bejot.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.bejot.sfgpetclinic.model.Vet;
+import pl.bejot.sfgpetclinic.services.VetService;
+
+import java.util.Set;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/index", "/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        Set<Vet> vets = vetService.findAll();
+        model.addAttribute("vets", vets);
+
         return "vets/index";
     }
 }
