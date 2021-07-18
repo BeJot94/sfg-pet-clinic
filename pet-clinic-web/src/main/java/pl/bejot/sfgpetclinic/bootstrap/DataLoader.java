dd3cacf111a9ab1ddc0eb0e716ccff6ef1f0f763
BeJot94 <bartosz.jasinski.94@gmail.com>
2021-07-18 13:37:29 +0200

    Refactoring Map services to generate IDs

--- pet-clinic-web/src/main/java/pl/bejot/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/pl/bejot/sfgpetclinic/bootstrap/DataLoader.java
@@ -20,14 +20,12 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner = new Owner();
-        owner.setId(1L);
         owner.setFirstName("Robert");
         owner.setLastName("Downey Junior");
 
         ownerService.save(owner);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Chris");
         owner2.setLastName("Evans");
 
@@ -36,14 +34,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded owners...");
 
         Vet vet = new Vet();
-        vet.setId(1L);
         vet.setFirstName("Chris");
         vet.setLastName("Hemsworth");
 
         vetService.save(vet);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Scarlett");
         vet2.setLastName("Johansson");
 
